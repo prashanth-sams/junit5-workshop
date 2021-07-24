da37f91e974f7927db2e7c4be22c87bc0a887b82
prashanth-sams <sams.prashanth@gmail.com>
2021-07-24 16:53:19 +0400

    dynamic test - iterable

---
+++ src/test/java/com/junit5/dynamictests/ReverseStringIterableTest.java
@@ -0,0 +1,49 @@
+package com.junit5.dynamictests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class ReverseStringIterableTest {
+
+    ReverseString reverseString = new ReverseString();
+
+    @TestFactory
+    @DisplayName("Reverse String Iterable Test")
+    public Iterable<DynamicTest> testReverseStringIterable() {
+
+        List<String> inputList = createInputList();
+        List<String> outputList = createOutputList();
+
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int i = 0; i < inputList.size(); i++) {
+            String input = inputList.get(i);
+            String output = outputList.get(i);
+
+            DynamicTest dynamicTest = dynamicTest("Dynamic Test for reverse() method with input: " + input, () -> {
+                assertEquals(output, reverseString.reverse(input));
+            });
+
+            dynamicTests.add(dynamicTest);
+        }
+
+        return dynamicTests;
+    }
+
+    private List<String> createInputList() {
+        return Arrays.asList("car", "bike", "cycle");
+    }
+
+    private List<String> createOutputList() {
+        return Arrays.asList("rac", "ekib", "elcyc");
+    }
+}
